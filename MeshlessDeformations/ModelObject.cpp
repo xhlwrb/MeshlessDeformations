8741f7dd2c18853ac595518abd298b6f2b439edb
xhlwrb <xhlwrb@hotmail.com>
2016-05-07 18:34:12 +0800

    Update ModelObject.cpp

--- MeshlessDeformations/ModelObject.cpp
+++ MeshlessDeformations/ModelObject.cpp
@@ -7,6 +7,8 @@ using namespace Model;
 //CONSTRUCTORS
 ModelObject::ModelObject(ObjectType::ObjectType type) : scene::ISceneNode(Globals::sceneManager->getRootSceneNode(), Globals::sceneManager, (type | ObjectType::ModelObjectType))
 {
+	// getID() 
+	// Get the id of the scene node. This id can be used to identify the node.
 	this->type = (ObjectType::ObjectType) getID();
 	this->isSelected = false;
 	
@@ -22,10 +24,12 @@ void ModelObject::OnPreRender()
 {
 	if (IsVisible) {
 		// Draw this node
+		// Registers a node for rendering it at a specific time.
 		SceneManager->registerNodeForRendering(this);
 
 
 		// Draw the children nodes
+		// Called after the scene's light list has been built, but before rendering has begun.
 		ISceneNode::OnPreRender();
 
 	}
@@ -39,9 +43,13 @@ void ModelObject::render()
 
 }
 
-
+// aabbox3d
+// Axis aligned bounding box in 3d dimensional space.
+// getBoundingBox()
+// Get the axis aligned, not transformed bounding box of this node.
 const core::aabbox3d<f32>& ModelObject::getBoundingBox() const
 {
+	// The bounding box of this mesh.
 	return Box;
 }
 
